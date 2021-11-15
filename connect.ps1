79f292b541c7ae282c72fce813d577ef689e03c7
Dana Gertsch <dgertsch@kovarus.com>
2021-11-15 16:33:01 -0700

    List ENV

--- connect.ps1
+++ connect.ps1
@@ -1,6 +1,8 @@
 # Disable SSL checking
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope AllUsers
 
+# print out ENV
+Get-ChildItem Env:
 # connect using Env. 
 Connect-VIserver -User $Env:vCenterUser -Password $Env:vCenterPass -Server $Env:vCenter 
 
