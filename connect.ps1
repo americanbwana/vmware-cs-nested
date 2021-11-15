f80c6f7eedb460a7eabc3afda7762ec3969d4f1b
Dana Gertsch <dgertsch@kovarus.com>
2021-11-15 16:17:22 -0700

    Disabled SSL check and now using ENV for Auth

--- connect.ps1
+++ connect.ps1
@@ -1,1 +1,5 @@
-Connect-VIserver
+# Disable SSL checking
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope AllUsers
+
+# connect using Env. 
+Connect-VIserver -User $Env:vCenter-User -Password $Env:vCenter-Pass -Server $Env:vCenter 
